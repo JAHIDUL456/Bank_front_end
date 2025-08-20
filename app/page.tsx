6b28c58bf07b1e41dccd01b275b3385b6b809771
JAHIDUL456 <1904015@student.ruet.ac.bd>
2025-08-20 15:38:43 +0600

    jaki

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,42 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import prime from '@/public/prime.png'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div>
+      {/* This is the main div  */}
+      
+      <div>
+        {/* this is the container div  */}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className='flex justify-between items-center bg-blue-50'>
+          {/* this div is for the header  */}
+          <Link href="/">
+          <Image src={prime} alt='logo' width={50} className='cursor-pointer ml-2'></Image>
+          
+          </Link>
+
+          <h1 className='text-sm font-semibold mr-6'>Queue Service</h1>
+          
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+      
+      {/* this portion is for the company branding  */}
+
+      <div className='bg-blue-100 mt-5 flex flex-col justify-center items-center w-[90%] ml-5 p-2 pb-5 rounded-xl'>
+        <h1 className='text-3xl font-bold text-blue-600 py-2'>Welcome to Prime Bank</h1>
+        <h1 className='text-xl font-semibold'>Gulshan Branch</h1>
+      </div>
+
+
+
+
+
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
